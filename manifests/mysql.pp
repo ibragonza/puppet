ba7bfa370ad80947aa91f2a446c196dc97ec86ce
Joris Vankemmel <joris.vankemmel@student.ucll.be>
2017-06-09 15:23:20 +0200

    Move grant to seperate function

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,17 +1,29 @@
-define mysqldb( $user, $password ) {
+define mysqldb_db( $user, $password ) {
     exec { "create-${name}-db":
-      unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
-      command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${name}; grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
+      command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${name};\"",
       require => Service["mysql"],
     }
 }
 
+define mysqldb_user( $user, $password ) {
+    exec { "create-$user":
+      unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
+      command => "/usr/bin/mysql -uroot -p$mysql_password -e \"grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
+      require => Service["mysql"],
+    }
+}
 
 class twspeeds::db {
-    mysqldb { "twspeeds":
+    mysqldb_db { "twspeeds":
       user => "twspeeds_admin",
       password => "testing_only",
     }
+    mysqldb_user { "twspeeds":
+      user => "twspeeds_admin",
+      password => "testing_only",
+    }
+
+
 }
 
 
